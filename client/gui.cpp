1141ae043b3d22ed0143d93ce6e69bc0debbcc23
Nigel Karunaratne <nigel.karunaratne36@gmail.com>
2025-04-04 11:30:21 -0400

    Add custom GUI functions

--- client/gui.cpp
+++ client/gui.cpp
@@ -1,14 +1,16 @@
 #include "gui.h"
 #include "string.h"
 
-bool GUI::TextBox(Rectangle bounds, char *textBuffer, Vector2 mousePos)
+#include <iostream>
+
+bool GUI::TextBox(Rectangle bounds, char *textBuffer, int bufferSize, Vector2 mousePos)
 {
     DrawRectangleLinesEx(bounds, 1.0f, BLACK);
     Vector2 textSize = MeasureTextEx(GetFontDefault(), textBuffer, 32, 1.0f);
 
     if(CheckCollisionPointRec(mousePos, bounds))
     {
-        DrawText(textBuffer, bounds.x + 5, bounds.y + (textSize.y / 2), 32, BLACK);
+        DrawText(textBuffer, bounds.x + 5, bounds.y + (bounds.height / 2) - (textSize.y / 2), 32, BLACK);
 
         int codepoint = GetCharPressed();
         int backSpacePressed = IsKeyPressed(KEY_BACKSPACE);
@@ -21,7 +23,7 @@ bool GUI::TextBox(Rectangle bounds, char *textBuffer, Vector2 mousePos)
         {
             textBuffer[strlen(textBuffer) - 1] = '\0';
         }
-        else if(strlen(textBuffer) < sizeof(textBuffer) && codepoint >= 32) //can concat
+        else if(strlen(textBuffer) < bufferSize - 1 && codepoint >= 32) //can concat
         {
             strcat(textBuffer, charEncoded);
         }
@@ -30,7 +32,24 @@ bool GUI::TextBox(Rectangle bounds, char *textBuffer, Vector2 mousePos)
     }
     else
     {
-        DrawText(textBuffer, bounds.x + 5, bounds.y + (textSize.y / 2), 32, GRAY);
+        DrawText(textBuffer, bounds.x + 5, bounds.y + (bounds.height / 2) - (textSize.y / 2), 32, GRAY);
+    }
+    return false;
+}
+
+bool GUI::TextButton(Rectangle bounds, const char *textToShow, Vector2 mousePos, Color buttonColor, Color borderColor)
+{
+    DrawRectangleLinesEx(bounds, 1.0f, borderColor);
+    DrawRectangleRec(bounds, buttonColor);
+    Vector2 textSize = MeasureTextEx(GetFontDefault(), textToShow, 32, 1.0f);
+    if(CheckCollisionPointRec(mousePos, bounds))
+    {
+        DrawText(textToShow, bounds.x + (bounds.width / 2) - (textSize.x / 2), bounds.y + (bounds.height / 2) - (textSize.y / 2), 32, BLACK);
+        if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) return true;
+    }
+    else
+    {
+        DrawText(textToShow, bounds.x + (bounds.width / 2) - (textSize.x / 2), bounds.y + (bounds.height / 2) - (textSize.y / 2), 32, GRAY);
     }
     return false;
 }
--- client/include/gui.h
+++ client/include/gui.h
@@ -7,6 +7,6 @@ class GUI
 {
 public:
     // PLACE IN A DRAW LOOP, AS THESE ASSUME DRAWING IS ACTIVE
-    static bool TextBox(Rectangle bounds, char* textBuffer, Vector2 mousePos);
-    static bool TextButton();
+    static bool TextBox(Rectangle bounds, char* textBuffer, int bufferSize, Vector2 mousePos);
+    static bool TextButton(Rectangle bounds, const char* textToShow, Vector2 mousePos, Color buttonColor, Color borderColor);
 };
